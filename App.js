09bdb8aeca85949445649c46d3f8e242a45cbef0
Geunhwa Lee <geunhwa.lee.01@gmail.com>
2021-07-11 16:51:27 +0900

    feat: 아이콘 추가 및 화면 레이아웃 변경

--- App.js
+++ App.js
@@ -14,10 +14,15 @@ export default class extends React.Component {
   };
 
   getWeather = async (latitude, longitude) => {
-    const { data } = await axios.get(
+    const {
+      data: {
+        main: { temp },
+        weather,
+      },
+    } = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     );
-    this.setState({ isLoading: false, temp: data.main.temp });
+    this.setState({ isLoading: false, condition: weather[0].main, temp });
   };
 
   getLocation = async () => {
@@ -37,7 +42,11 @@ export default class extends React.Component {
   }
 
   render() {
-    const { isLoading, temp } = this.state;
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
+    const { isLoading, condition, temp } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
   }
 }
